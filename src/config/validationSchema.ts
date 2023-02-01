89555609c0ee70249cce178bf8e1235d0c71d3f6
MINGYU KIM <84526709+k0136000@users.noreply.github.com>
2023-02-01 19:54:50 +0900

    Delete src/config directory

--- src/config/validationSchema.ts
+++
@@ -1,8 +0,0 @@
-import * as Joi from 'joi';
-
-export const validationSchema = Joi.object({
-  EMAIL_SERVICE: Joi.string().required(),
-  EMAIL_BASE_URL: Joi.string().required().uri(),
-  AUTHEMAIL: Joi.string().required(),
-  AUTHEMAILPASSWORD: Joi.string().required(),
-});
